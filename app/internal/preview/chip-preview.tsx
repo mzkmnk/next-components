00f77148878ae1d31b1fc87b6006edd65a7cb375
mzkmnk <sma0729mex@gmail.com>
2024-10-14 22:06:13 +0900

    chore:w-full

--- app/internal/preview/chip-preview.tsx
+++ app/internal/preview/chip-preview.tsx
@@ -6,7 +6,7 @@ const ChipPreview = () => {
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <h2 className="text-left w-full">Chip</h2>
-            <div className="flex flex-row justify-center items-center gap-2 border rounded-xl w-full p-3">
+            <div className="flex flex-row justify-center items-center gap-2 border rounded-xl p-5">
                 <Chip variant={'primary'}>primary</Chip>
                 <Chip variant={'secondary'}>secondary</Chip>
                 <Chip variant={'warning'}>waring</Chip>
