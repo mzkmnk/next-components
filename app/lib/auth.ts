ec9917a1497fbadfb299369c7747e19dfaf07967
sma0729mex <sma0729mex@gmail.com>
2024-10-21 03:12:50 +0000

    chore:linter redirect

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { NextRequest, NextResponse } from "next/server";
 
 export const {handlers,auth,signIn,signOut} = NextAuth({
     providers:[GitHub],
