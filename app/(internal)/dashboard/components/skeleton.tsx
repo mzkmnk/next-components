16846d86e059eb14b6c6c6cd3a708824aec7eea4
mzkmnk <sma0729mex@gmail.com>
2024-10-22 22:19:40 +0900

    chore:skeleton ui

--- app/(internal)/dashboard/components/skeleton.tsx
+++ app/(internal)/dashboard/components/skeleton.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils"
 
 export const Skeleton = ({className}:{className:string}) => {
     return (
-        <div className={cn("animate-pulse rounded-md bg-slate-200",className)}>
+        <div className={cn("animate-pulse rounded-xl bg-gray-200",className)}>
         </div>
     )
 }
