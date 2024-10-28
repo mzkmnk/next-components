0503282b107e3051b903e04ba5866cb97e20a633
mzkmnk <sma0729mex@gmail.com>
2024-10-28 22:46:20 +0900

    log3

--- app/(internal)/dashboard/repositories/[repositoryName]/page.tsx
+++ app/(internal)/dashboard/repositories/[repositoryName]/page.tsx
@@ -8,7 +8,7 @@ const Page = async ({params}:{params:Promise<{repositoryName:string}>}) => {
 
     const headersData = await headers();
     const host = headersData.get('host');
-    const protocol = headersData.get('x-forwarded-proto') ?? host?.startsWith('localhost') ? 'http' : 'https';
+    const protocol = headersData.get('x-forwarded-proto');
 
     console.dir(headersData,{depth:null});
 
