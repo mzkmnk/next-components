f86f29a5b7b505371cd39662bb2ff4317abc943d
mzkmnk <sma0729mex@gmail.com>
2024-10-28 23:09:27 +0900

    log5

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
-import {auth} from '@/lib/auth';
-
 // export { auth } from '@/lib/auth';
 
-export default auth((req) => {
-    if(req.auth && req.url.startsWith(`${process.env.API_PREFIX}/sign-in`)){
-        return Response.redirect(`${process.env.API_PREFIX}/dashboard/repositories/next-components`);
-    }
-})
+// export default auth((req) => {
+//     if(req.auth && req.url.startsWith(`${process.env.API_PREFIX}/sign-in`)){
+//         return Response.redirect(`${process.env.API_PREFIX}/dashboard/repositories/next-components`);
+//     }
+// })
 
 export const config = {
     matcher:["/((?!api|_next/static|_next/image|favicon.ico).*)"]
